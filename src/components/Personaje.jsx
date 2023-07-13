ca0efdc75311114e2b4064f6a9700d3b410a77f3
Fer <fernanda23demarzo@gmail.com>
2023-07-12 23:57:12 -0300

    Mejora código

--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -1,14 +1,13 @@
-
+import '../App.css';
 
 export default function Personaje({personaje}) {
+
   return (
 
-    <>
     <div>
-    <h2>{personaje.name}</h2>
+    <h2 className='nombre'>{personaje.name}</h2>
     <img src={personaje.image} alt={personaje.name}></img>
     </div>
-    </>
    
   );
 }
--- src/components/Personajes.jsx
+++ src/components/Personajes.jsx
@@ -1,35 +1,49 @@
-import { Container } from "react-bootstrap"
 import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 import Personaje from "./Personaje";
+import SiguientePag from "./Botones";
+import Buscador from "./Buscador";
 
 
 export default function Personajes() {
 
-    const [personaje, setPersonaje] = useState([])
+  const [personaje, setPersonaje] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [filtro, setFiltro] = useState("");
 
-    useEffect(() => {
-      async function datosFetch(){
-      const respuesta = await fetch('https://rickandmortyapi.com/api/character');
+
+  useEffect(() => {
+    async function datosFetch() {
+      const respuesta = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${pagina}`
+      );
       const data = await respuesta.json();
       setPersonaje(data.results);
-      }
-  
-      datosFetch()
-    }, []);
+    }
 
+    datosFetch();
+    
+  }, [pagina]);
 
+  const filtrarPersonajes = (personajes) => {
+    return personajes.filter((personaje) =>
+      personaje.name.toLowerCase().includes(filtro.toLowerCase())
+    );
+  };
 
   return (
-    
     <Container>
-        {
-        personaje.map((personaje) => {
-          return(
-            <Personaje personaje={personaje} key={personaje.id}/>
-          )
-        })
-      }
+
+      <Buscador filtro={filtro} setFiltro={setFiltro} />
+      <div className="row">
+        {filtrarPersonajes(personaje).map((personaje) => (
+          <div className="col-md-4" key={personaje.id}>
+            <Personaje personaje={personaje} />
+          </div>
+        ))}
+      </div>
+
+      <SiguientePag pagina={pagina} setPagina={setPagina} />
     </Container>
-    
-  )
+  );
 }
